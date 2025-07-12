23cec88d9c3f127242e9957b7831254f918704d2
Faded <granveer308@gmail.com>
2025-07-12 14:23:29 +1200

    Update init.lua

--- Lua/init.lua
+++ Lua/init.lua
@@ -1,1 +1,9 @@
+getgenv().identifyexecutor = function()
+    return "ParadiseWRD"
+end
 
+getgenv().getexecutorname = function()
+    return "ParadiseWRD"
+end
+
+print("Environment Loaded! | discord.gg/getparadise
